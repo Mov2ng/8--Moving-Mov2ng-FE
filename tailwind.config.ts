88b3aab7d5297398fdc7ced0fe88e3654de9d397
MyeongjuHong <hmjpearl@gmail.com>
2025-12-05 10:00:37 +0900

    style: 색상 적용 수정 (config -> globals)
    - 현재 css v4 버전의 권장 방식이 css 파일에서의 직접 정의기 때문에 theme 지시어를 사용해 css 변수로 사용할 수 있도록 변경했습니다
    - 결과적으로 색상은 globals.css의 @theme에서 관리하고, tailwind.config.ts는 파일 스캔 경로만 지정하도록 변경했습니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,55 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  theme: {
-    extend: {
-      colors: {
-        black: {
-          100: "#6B6B6B",
-          200: "#525252",
-          300: "#373737",
-          400: "#1F1F1F",
-          500: "#040404",
-        },
-        gray: {
-          50: "#FFFFFF",
-          100: "#DEDEDE",
-          200: "#C4C4C4",
-          300: "#ABABAB",
-          400: "#999999",
-          500: "#808080",
-        },
-        primary: {
-          blue: {
-            50: "#F5FAFF",
-            100: "#E9F4FF",
-            200: "#4DA9FF",
-            300: "#1B92FF",
-            400: "#242945",
-          },
-        },
-        secondary: {
-          yellow: {
-            100: "#FFC149",
-          },
-          red: {
-            100: "#FFEEF0",
-            200: "#FF4F64",
-          },
-        },
-        background: {
-          100: "#FAFAFA",
-          200: "#F7F7F7",
-          300: "#EFEFEF",
-          400: "#F4F7FB",
-        },
-        line: {
-          100: "#F2F2F2",
-          200: "#E6E6E6",
-        },
-      },
-    },
-  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
 };
 
 export default config;
