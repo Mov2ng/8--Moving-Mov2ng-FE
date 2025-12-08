f49023a8f288d7b5545ff4b998fbafbdc6f59368
MyeongjuHong <hmjpearl@gmail.com>
2025-12-08 18:37:04 +0900

    feat: Provider 수정
    - userContext 삭제 (context api 대신 react query로 유저 상태 관리 가능)
    - layout에 바로 Provider 컴포넌트 삽입

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React from 'react';
-import { UserProvider } from '@/contexts/UserContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { I18nProvider } from './i18n/I18nProvider';
+import { I18nProvider } from '../libs/i18n/I18nProvider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,11 +17,10 @@ const queryClient = new QueryClient({
 export function Provider({ children }: { children: React.ReactNode }) { // 모든 페이지에 적용되는 공통 레이아웃
   return (
     <QueryClientProvider client={queryClient}> {/* queryClient를 사용하여 데이터 캐싱 */}
-      <UserProvider> {/* UserContext를 사용하여 로그인 유저 정보 관리 */}
         <I18nProvider> {/* I18nContext를 사용하여 언어 설정 관리 */}
         {children}
+        <ReactQueryDevtools initialIsOpen={false} />
         </I18nProvider>
-      </UserProvider>
     </QueryClientProvider>
   )
 }
