ab8d841631cc037e6627b29022cb69e2b8170f40
DESKTOP-14A5R6M\clwns <clwns00@naver.com>
2025-12-24 11:22:03 +0900

    feat/작성 가능한 리뷰 ui &  내가 작성한 리뷰 ui 작성

---
+++ src/types/view/review.ts
@@ -0,0 +1,82 @@
+export type ReviewItem = {
+  id: number;
+  serviceType: string;
+  isDesignatedRequest?: boolean;
+  designatedLabel?: string;
+  name: string;
+  profileImage: string;
+  movingDate: string;
+  price: number;
+  reviewEnabled?: boolean;
+  reviewButtonText?: string;
+};
+
+export type ApiWritableReview = {
+  id: number;
+  price?: number;
+  request: {
+    moving_data: string;
+    moving_type?: string;
+  };
+  driver: {
+    nickname?: string;
+    profileImage?: string;
+    user?: {
+      name?: string;
+    };
+  };
+};
+
+export type ApiWrittenReview = {
+  id: number;
+  price?: number;
+  driver_id?: number;
+  user_id?: string;
+  review_title?: string;
+  review_content?: string;
+  rating?: number;
+  createdAt?: string;
+  request?: {
+    moving_type?: string;
+    moving_data?: string;
+    price?: number;
+    isDesignatedRequest?: boolean;
+  };
+  driver: {
+    nickname?: string;
+    driver_years?: number;
+    driver_intro?: string;
+    driver_content?: string;
+    profileImage?: string;
+    user?: {
+      name?: string;
+      email?: string;
+      phone_number?: string;
+    };
+    estimates?: {
+      id: number;
+      price?: number;
+      status?: string;
+      request?: {
+        id?: number;
+        moving_type?: string;
+        moving_data?: string;
+      };
+    }[];
+  };
+};
+
+export type ReviewWrittenItem = {
+  id: number;
+  serviceType: string;
+  isDesignatedRequest?: boolean;
+  designatedLabel?: string;
+  createdAt?: string;
+  name: string;
+  profileImage: string;
+  movingDate: string;
+  price: number;
+  rating: number;
+  reviewText: string;
+  reviewTitle?: string;
+};
