b9417718783fb7a01ea2061a9ffc2837c3207ed5
MyeongjuHong <hmjpearl@gmail.com>
2025-12-29 04:05:19 +0900

    chore: apiClient, useApiQuery 파일에 JSDocs 주석 설명 추가

--- src/hooks/useApiMutation.ts
+++ src/hooks/useApiMutation.ts
@@ -1,14 +1,21 @@
-/** useMutation을 api 호출에 맞게 래핑, 데이터 변경 작업(POST, PUT, DELETE) 처리, apiClient 통합, 성공/에러 처리 통일
- */
 "use client";
 
 import {
   MutationFunction,
-  MutationFunctionContext,
   useMutation,
   UseMutationOptions,
 } from "@tanstack/react-query";
 
+/** 
+ * React Query mutation 래퍼
+ * - useMutation을 API 호출에 맞게 래핑
+ * - 데이터 변경 작업(POST, PUT, DELETE) 처리
+ * - API 호출을 apiClient로 통합
+ * - 성공/에러 처리 통일
+ * @param mutationFn - 뮤테이션 함수
+ * @param options - 뮤테이션 옵션
+ * @returns useMutation 결과
+ */
 export function useApiMutation<TData, TVariables, TError>({
   mutationFn,
   ...options
--- src/hooks/useApiQuery.ts
+++ src/hooks/useApiQuery.ts
@@ -11,6 +11,18 @@ import {
   UseQueryOptions,
 } from "@tanstack/react-query";
 
+/**
+ * React Query query 래퍼
+ * - useQuery를 API 호출에 맞게 래핑
+ * - API 호출을 apiClient로 통합
+ * - 성공/에러 처리 통일
+ * @param queryKey - 쿼리 키
+ * @param queryFn - 쿼리 함수
+ * @param enabled - 쿼리 활성화 여부
+ * @param staleTime - 캐시 유효 시간
+ * @param options - 쿼리 옵션
+ * @returns useQuery 결과
+ */
 export function useApiQuery<TData, TError>({
   queryKey,
   queryFn,
