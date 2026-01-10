85412fd76c196d90b727831145ffc244a6d7c2a4
MyeongjuHong <hmjpearl@gmail.com>
2026-01-10 21:23:48 +0900

    fix: import 파일명 대문자 시작으로 변경
    - estimatePage to EstimatePage

--- src/app/estimate/driver/(components)/EstimatePageDetail.tsx
+++ src/app/estimate/driver/(components)/EstimatePageDetail.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useState } from "react";
-import EstimatePage from "./estimatePage"; 
+import EstimatePage from "./EstimatePage"; 
 export default function EstimatePageDetail() {
   return (
     <EstimatePage />
